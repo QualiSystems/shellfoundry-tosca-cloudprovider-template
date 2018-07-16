9b3118371a8afeba0df451074644a7a8900c4308
alexazarh <alex.az@quali.com>
2018-07-16 19:02:25 +0300

    Updated the driver to have relevant commands for cloud provider standards

--- {{cookiecutter.project_slug}}/src/driver.py
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -1,13 +1,7 @@
-import datetime
-import json
-
-from cloudshell.networking.apply_connectivity.apply_connectivity_operation import apply_connectivity_changes
-from cloudshell.networking.apply_connectivity.models.connectivity_result import ConnectivitySuccessResponse
-from cloudshell.shell.core.interfaces.save_restore import OrchestrationSaveResult, OrchestrationSavedArtifact, \
-    OrchestrationSavedArtifactInfo, OrchestrationRestoreRules
-from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
-    AutoLoadAttribute, AutoLoadDetails, CancellationContext
+from cloudshell.cp.core import DriverRequestParser
+from cloudshell.cp.core.models import DriverResponse
+from cloudshell.shell.core.driver_context import InitCommandContext, AutoLoadCommandContext, ResourceCommandContext, \
+    AutoLoadAttribute, AutoLoadDetails, CancellationContext, ResourceRemoteCommandContext
 
 
 #from data_model import *  # run 'shellfoundry generate' to generate data model classes
@@ -18,7 +12,7 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         """
         ctor must be without arguments, it is created with reflection at run time
         """
-        pass
+        self.request_parser = DriverRequestParser()
 
     def initialize(self, context):
         """
@@ -27,175 +21,6 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         :param InitCommandContext context: the context the command runs on
         """
         pass
-
-    # <editor-fold desc="Networking Standard Commands">
-    def restore(self, context, cancellation_context, path, configuration_type, restore_method, vrf_management_name):
-        """
-        Restores a configuration file
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str path: The path to the configuration file, including the configuration file name.
-        :param str restore_method: Determines whether the restore should append or override the current configuration.
-        :param str configuration_type: Specify whether the file should update the startup or running config.
-        :param str vrf_management_name: Optional. Virtual routing and Forwarding management name
-        """
-        pass
-
-    def save(self, context, cancellation_context, folder_path, configuration_type, vrf_management_name):
-        """
-        Creates a configuration file and saves it to the provided destination
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str configuration_type: Specify whether the file should update the startup or running config. Value can one
-        :param str folder_path: The path to the folder in which the configuration file will be saved.
-        :param str vrf_management_name: Optional. Virtual routing and Forwarding management name
-        :return The configuration file name.
-        :rtype: str
-        """
-        pass
-
-    def load_firmware(self, context, cancellation_context, path, vrf_management_name):
-        """
-        Upload and updates firmware on the resource
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param str path: path to tftp server where firmware file is stored
-        :param str vrf_management_name: Optional. Virtual routing and Forwarding management name
-        """
-        pass
-
-    def run_custom_command(self, context, cancellation_context, custom_command):
-        """
-        Executes a custom command on the device
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str custom_command: The command to run. Note that commands that require a response are not supported.
-        :return: the command result text
-        :rtype: str
-        """
-        pass
-
-    def run_custom_config_command(self, context, cancellation_context, custom_command):
-        """
-        Executes a custom command on the device in configuration mode
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str custom_command: The command to run. Note that commands that require a response are not supported.
-        :return: the command result text
-        :rtype: str
-        """
-        pass
-
-    def shutdown(self, context, cancellation_context):
-        """
-        Sends a graceful shutdown to the device
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        """
-        pass
-
-    # </editor-fold>
-
-    # <editor-fold desc="Orchestration Save and Restore Standard">
-    def orchestration_save(self, context, cancellation_context, mode, custom_params):
-        """
-        Saves the Shell state and returns a description of the saved artifacts and information
-        This command is intended for API use only by sandbox orchestration scripts to implement
-        a save and restore workflow
-        :param ResourceCommandContext context: the context object containing resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str mode: Snapshot save mode, can be one of two values 'shallow' (default) or 'deep'
-        :param str custom_params: Set of custom parameters for the save operation
-        :return: SavedResults serialized as JSON
-        :rtype: OrchestrationSaveResult
-        """
-
-        # See below an example implementation, here we use jsonpickle for serialization,
-        # to use this sample, you'll need to add jsonpickle to your requirements.txt file
-        # The JSON schema is defined at: https://github.com/QualiSystems/sandbox_orchestration_standard/blob/master/save%20%26%20restore/saved_artifact_info.schema.json
-        # You can find more information and examples examples in the spec document at https://github.com/QualiSystems/sandbox_orchestration_standard/blob/master/save%20%26%20restore/save%20%26%20restore%20standard.md
-        '''
-        # By convention, all dates should be UTC
-        created_date = datetime.datetime.utcnow()
-
-        # This can be any unique identifier which can later be used to retrieve the artifact
-        # such as filepath etc.
-
-        # By convention, all dates should be UTC
-        created_date = datetime.datetime.utcnow()
-
-        # This can be any unique identifier which can later be used to retrieve the artifact
-        # such as filepath etc.
-        identifier = created_date.strftime('%y_%m_%d %H_%M_%S_%f')
-
-        orchestration_saved_artifact = OrchestrationSavedArtifact('REPLACE_WITH_ARTIFACT_TYPE', identifier)
-
-        saved_artifacts_info = OrchestrationSavedArtifactInfo(
-            resource_name="some_resource",
-            created_date=created_date,
-            restore_rules=OrchestrationRestoreRules(requires_same_resource=True),
-            saved_artifact=orchestration_saved_artifact)
-
-        return OrchestrationSaveResult(saved_artifacts_info)
-        '''
-        pass
-
-    def orchestration_restore(self, context, cancellation_context, saved_artifact_info, custom_params):
-        """
-        Restores a saved artifact previously saved by this Shell driver using the orchestration_save function
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str saved_artifact_info: A JSON string representing the state to restore including saved artifacts and info
-        :param str custom_params: Set of custom parameters for the restore operation
-        :return: None
-        """
-        '''
-        # The saved_details JSON will be defined according to the JSON Schema and is the same object returned via the
-        # orchestration save function.
-        # Example input:
-        # {
-        #     "saved_artifact": {
-        #      "artifact_type": "REPLACE_WITH_ARTIFACT_TYPE",
-        #      "identifier": "16_08_09 11_21_35_657000"
-        #     },
-        #     "resource_name": "some_resource",
-        #     "restore_rules": {
-        #      "requires_same_resource": true
-        #     },
-        #     "created_date": "2016-08-09T11:21:35.657000"
-        #    }
-
-        # The example code below just parses and prints the saved artifact identifier
-        saved_details_object = json.loads(saved_details)
-        return saved_details_object[u'saved_artifact'][u'identifier']
-        '''
-        pass
-
-    # </editor-fold>
-
-    # <editor-fold desc="Connectivity Provider Interface (Optional)">
-
-    '''
-    # The ApplyConnectivityChanges function is intended to be used for using switches as connectivity providers
-    # for other devices. If the Switch shell is intended to be used a DUT only there is no need to implement it
-
-    def ApplyConnectivityChanges(self, context, request):
-        """
-        Configures VLANs on multiple ports or port-channels
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param str request: A JSON object with the list of requested connectivity changes
-        :return: a json object with the list of connectivity changes which were carried out by the switch
-        :rtype: str
-        """
-
-        return apply_connectivity_changes(request=request,
-                                          add_vlan_action=lambda x: ConnectivitySuccessResponse(x,'Success'),
-                                          remove_vlan_action=lambda x: ConnectivitySuccessResponse(x,'Success'))
-
-
-
-    '''
-
-    # </editor-fold>
 
     # <editor-fold desc="Discovery">
 
@@ -207,42 +32,154 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         :rtype: AutoLoadDetails
         """
 
-        # See below some example code demonstrating how to return the resource structure and attributes
-        # In real life, this code will be preceded by SNMP/other calls to the resource details and will not be static
         # run 'shellfoundry generate' in order to create classes that represent your data model
 
-        '''
-        resource = {{cookiecutter.model_name}}.create_from_context(context)
-        resource.vendor = 'specify the shell vendor'
-        resource.model = 'specify the shell model'
-
-        chassis1 = GenericChassis('Chassis 1')
-        chassis1.model = 'WS-X4232-GB-RJ'
-        chassis1.serial_number = 'JAE053002JD'
-        resource.add_sub_resource('1', chassis1)
-
-        module1 = GenericModule('Module 1')
-        module1.model = 'WS-X5561-GB-AB'
-        module1.serial_number = 'TGA053972JD'
-        chassis1.add_sub_resource('1', module1)
-
-        port1 = GenericPort('Port 1')
-        port1.mac_address = 'fe80::e10c:f055:f7f1:bb7t16'
-        port1.ipv4_address = '192.168.10.7'
-        module1.add_sub_resource('1', port1)
-
-        return resource.create_autoload_details()
-        '''
         return AutoLoadDetails([], [])
 
     # </editor-fold>
 
-    # <editor-fold desc="Health Check">
+    # <editor-fold desc="Mandatory Commands">
 
-    def health_check(self,cancellation_context):
+    def Deploy(self, context, request=None, cancellation_context=None):
         """
-        Checks if the device is up and connectable
-        :return: str: Success or fail message
+        Deploy
+        :param ResourceCommandContext context:
+        :param str request: A JSON string with the list of requested deployment actions
+        :param CancellationContext cancellation_context:
+        :return:
+        :rtype: str
+        """
+
+        '''
+        # parse the json strings into action objects
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        
+        # extract DeployApp action
+        deploy_action = single(actions, lambda x: isinstance(x, DeployApp))
+        
+        # if we have multiple supported deployment options use the 'deploymentPath' property 
+        # to decide which deployment option to use. 
+        deployment_name = deploy_action.actionParams.deployment.deploymentPath
+        
+        deploy_result = _my_deploy_method(context, actions, cancellation_context)
+        return DriverResponse(deploy_result).to_driver_response_json()
+        '''
+        pass
+
+    def PowerOn(self, context, ports):
+        """
+        Will power on the compute resource
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        """
+        pass
+
+    def PowerOff(self, context, ports):
+        """
+        Will power off the compute resource
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        """
+        pass
+
+    def DeleteInstance(self, context, ports):
+        """
+        Will delete the compute resource
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        """
+        pass
+
+    def GetVmDetails(self, context, requests, cancellation_context):
+        """
+
+        :param ResourceCommandContext context:
+        :param str requests:
+        :param CancellationContext cancellation_context:
+        :return:
+        """
+        pass
+
+    def remote_refresh_ip(self, context, ports, cancellation_context):
+        """
+        Will update the address of the computer resource on the Deployed App resource in cloudshell
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        :param CancellationContext cancellation_context:
+        :return:
+        """
+        pass
+
+    # </editor-fold>
+
+
+    ### NOTE: According to the Connectivity Type of your shell, remove the commands that are not
+    ###       relevant from this file and from drivermetadata.xml.
+
+    # <editor-fold desc="Mandatory Commands For L2 Connectivity Type">
+
+    def ApplyConnectivityChanges(self, context, request):
+        """
+        Configures VLANs on multiple ports or port-channels
+        :param ResourceCommandContext context: The context object for the command with resource and reservation info
+        :param str request: A JSON string with the list of requested connectivity changes
+        :return: a json object with the list of connectivity changes which were carried out by the driver
+        :rtype: str
+        """
+        pass
+
+    # </editor-fold> 
+
+    # <editor-fold desc="Mandatory Commands For L3 Connectivity Type">
+
+    def PrepareSandboxInfra(self, context, request, cancellation_context):
+        """
+
+        :param ResourceCommandContext context:
+        :param str request:
+        :param CancellationContext cancellation_context:
+        :return:
+        :rtype: str
+        """
+        '''
+        # parse the json strings into action objects
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        
+        action_results = _my_prepare_connectivity(context, actions, cancellation_context)
+        
+        return DriverResponse(action_results).to_driver_response_json()    
+        '''
+        pass
+
+    def CleanupSandboxInfra(self, context, request):
+        """
+
+        :param ResourceCommandContext context:
+        :param str request:
+        :return:
+        :rtype: str
+        """
+        '''
+        # parse the json strings into action objects
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        
+        action_results = _my_cleanup_connectivity(context, actions)
+
+        return DriverResponse(action_results).to_driver_response_json()    
+        '''
+        pass
+
+    # </editor-fold>
+
+    # <editor-fold desc="Optional Commands For L3 Connectivity Type">
+
+    def SetAppSecurityGroups(self, context, request):
+        """
+
+        :param ResourceCommandContext context:
+        :param str request:
+        :return:
+        :rtype: str
         """
         pass
 
@@ -251,6 +188,6 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
     def cleanup(self):
         """
         Destroy the driver session, this function is called everytime a driver instance is destroyed
-        This is a good place to close any open sessions, finish writing to log files
+        This is a good place to close any open sessions, finish writing to log files, etc.
         """
         pass
